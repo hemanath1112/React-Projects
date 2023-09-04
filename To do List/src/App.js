f23b94c39a9539eff5040999d20bb68a8226790a
Hemanath Chakkaravarthi <114560429+hemanath1112@users.noreply.github.com>
2023-09-04 16:06:02 +0530

    updated

---
+++ To do List/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import NewComponets from "./NewComponets";
+
+function App() {
+  return (
+    <div className="App">
+      <NewComponets />
+    </div>
+  );
+}
+
+export default App;
